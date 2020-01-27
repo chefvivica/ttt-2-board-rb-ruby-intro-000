a0e1689cdf2b5e100d4e03d317ff54b33261ec93
Vivica Ma <vivicacongma@gmail.com>
2020-01-27 04:35:53 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,6 @@
-# Define the variable board below.
+row = ["",""," "]   # Define the variable board below.
+row[0]
+row[1]      
+row[2]
+row[1] = "x"
+board = ["0","1","2","3","4","5","6","7","8","9"]
